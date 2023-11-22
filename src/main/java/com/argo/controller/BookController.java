828d8991b634846313777e523d164f721053c82d
Yong <swjasonyang@163.com>
2023-11-23 00:47:01 +0800

    添加功能模块2

--- src/main/java/com/argo/controller/BookController.java
+++ src/main/java/com/argo/controller/BookController.java
@@ -4,8 +4,8 @@ package com.argo.controller;
 import com.argo.domain.Book;
 import com.argo.mapper.BookMapper;
 import com.argo.service.impl.service.BookService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -13,25 +13,30 @@ import java.util.List;
 @RequestMapping("/books")
 @RestController
 public class BookController {
-    @Resource
+    @Autowired
     BookService bookService;
 
-    public boolean save(Book book) {
+    @PostMapping
+    public boolean save(@RequestBody Book book) {
         return false;
     }
 
-    public boolean delete(Integer id) {
+    @DeleteMapping("/{id}")
+    public boolean delete(@PathVariable Integer id) {
         return false;
     }
 
-    public void update(Book book) {
+    @PutMapping
+    public void update(@RequestBody Book book) {
 
     }
 
-    public boolean getById(Integer id) {
+    @GetMapping("/{id}")
+    public boolean getById(@PathVariable Integer id) {
         return false;
     }
 
+    @GetMapping
     public List<Book> getAll() {
         return null;
     }
